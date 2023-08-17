a1123b07a60af10f2ed02b8cc06008dbfca6bb9b
Emre Ozer <constellation2023@gmail.com>
2023-08-17 11:34:26 +0300

    17/8/2023

--- Selenium Projects/mycucumberproject/src/test/java/runners/Runner.java
+++ Selenium Projects/mycucumberproject/src/test/java/runners/Runner.java
@@ -6,6 +6,13 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = { //report plugins
+                "pretty", //generates reports on the console
+                "html:target/default-cucumber-reports.html",
+                "json:target/json-reports/cucumber.json",
+                "junit:target/xml-report/cucumber.xml"
+        },
+        monochrome = true, //makes reports more readable in the console
         features = "./src/test/resources/features", //path of feature file
         glue = "stepdefinitions", //path of the step definitions
         tags = "@data_tables", //only runs this test case
