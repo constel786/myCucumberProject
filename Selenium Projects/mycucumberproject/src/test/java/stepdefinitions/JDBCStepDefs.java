2615eb06579d589f2161418da122ca8227ce9b9f
Emre Ozer <constellation2023@gmail.com>
2023-08-23 12:35:53 +0300

    23/8/2023

--- Selenium Projects/mycucumberproject/src/test/java/stepdefinitions/JDBCStepDefs.java
+++ Selenium Projects/mycucumberproject/src/test/java/stepdefinitions/JDBCStepDefs.java
@@ -1,11 +1,26 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import utilities.DBUtils;
+
+import java.sql.SQLException;
 
 public class JDBCStepDefs {
     @Given("user connects to the application database")
     public void user_connects_to_the_application_database() {
         DBUtils.createConnection();
+    }
+    @And("user gets the column {string} from the table {string}")
+    public void userGetsTheColumnFromTheTable(String column, String table) {
+        String query = "select "+column+" from "+table+"";
+        DBUtils.executeQuery(query);
+    }
+    @And("user reads all the column {string} data")
+    public void userReadsAllTheColumnData(String column) throws SQLException {
+//        going to the new row
+        DBUtils.getResultset().next(); //1st row
+        Object object1 = DBUtils.getResultset().getObject(column);//returns the data on that 1st row
+        System.out.println(object1.toString());//printing that data
     }
 }
--- Selenium Projects/mycucumberproject/src/test/resources/features/day24_jdbc.feature
+++ Selenium Projects/mycucumberproject/src/test/resources/features/day24_jdbc.feature
@@ -2,3 +2,5 @@
 Feature: jdbc_feature
   Scenario: TC01_read_customer_login_information
     Given user connects to the application database
+    And user gets the column "*" from the table "jhi_user"
+    And user reads all the column "login" data
