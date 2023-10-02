b34cd9ae08644c9d29da662c44ac8e57ffb6b83f
constel786 <134733862+constel786@users.noreply.github.com>
2023-10-02 09:46:14 +0300

    2/10/2023

--- Selenium Projects/mycucumberproject/src/test/java/stepdefinitions/JDBCStepDefs.java
+++ Selenium Projects/mycucumberproject/src/test/java/stepdefinitions/JDBCStepDefs.java
@@ -40,7 +40,7 @@ public class JDBCStepDefs {
     }
     @Then("verify table {string} and column {string} contains following data {string}")
     public void verifyTableAndColumnContainsFollowingData(String table, String column, String data) {
-//        String query = "select " + column + " from " + table;
+        String query = "select " + column + " from " + table;
 //        List<Object> columnList = DBUtils.getColumnData(query, column);
         System.out.println("columnList = " + columnList);
         Assert.assertTrue(columnList.contains(data));
